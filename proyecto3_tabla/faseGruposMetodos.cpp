5fcf8298356869477a324c75ba9438a79fd5c50c
ivanhernandezpaduano-glitch <ivanhernandezpaduano@gmail.com>
2026-06-09 22:58:13 -0600

    Update faseGruposMetodos.cpp

--- proyecto3_tabla/faseGruposMetodos.cpp
+++ proyecto3_tabla/faseGruposMetodos.cpp
@@ -8,8 +8,8 @@ GrupoMundial& FaseDeGrupos::operator[](const string& nombre) {
     return grupos.at(nombre);
 }
 
-void FaseDeGrupos::registrarPartidoEnGrupo(const string& grupo, const string& local, int gLocal,
-                                            const string& visitante, int gVisitante) {
+void FaseDeGrupos::registrarPartidoEnGrupo(const string& grupo, const string& local,
+                                           int gLocal, const string& visitante, int gVisitante) {
     try {
         grupos.at(grupo).registrarPartido(local, gLocal, visitante, gVisitante);
     } catch (const MundialException& e) {
